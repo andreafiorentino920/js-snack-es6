1dd5e3bf9a9fe537fc1d335d8b2529f0d9165e45
andreafiorentino920 <andreafiorentino920@gmail.com>
2021-09-09 17:45:39 +0100

    Push esercizio

--- snack 2/js/script.js
+++ snack 2/js/script.js
@@ -3,3 +3,41 @@
 // Generare numeri random al posto degli 0 nelle proprietà:
 // Punti fatti e falli subiti.
 // Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const generaNumeri = (max) => Math.floor(Math.random() * max )
+const squadre = [
+    {
+        nomeSquadra: "Juventus",
+        puntiFatti: generaNumeri(50),
+        falliSubiti: generaNumeri(80)
+    },
+    {
+        nomeSquadra: "Inter",
+        puntiFatti: generaNumeri(50),
+        falliSubiti: generaNumeri(80)
+        
+    },
+    {
+        nomeSquadra: "Lecce",
+        puntiFatti: generaNumeri(50),
+        falliSubiti: generaNumeri(80)
+    },
+    {
+        nomeSquadra: "Torino",
+        puntiFatti: generaNumeri(50),
+        falliSubiti: generaNumeri(80)
+    },
+    {
+        nomeSquadra: "Palermo",
+        puntiFatti: generaNumeri(50),
+        falliSubiti: generaNumeri(80)
+    }
+];
+console.log(squadre);
+var arrayNuovo = [];
+for(var i=0; i<=squadre.length;i++){
+    const [nome,,falli] = squadre[i];
+    arrayNuovo.push(nome,falli);
+}
+console.log(arrayNuovo);
+
